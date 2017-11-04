0d3eaae3fc53b42fd85a16ad6e56d120f15512a6
Omar Hamdan <omarhamdan1993@gmail.com>
2017-11-05 01:20:33 +0200

    setup for ex-1

--- app/router.js
+++ app/router.js
@@ -1,4 +1,4 @@
- import EmberRouter from '@ember/routing/router';
+import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
 const Router = EmberRouter.extend({
@@ -7,6 +7,7 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('main', { path: '/' });
 });
 
 export default Router;
